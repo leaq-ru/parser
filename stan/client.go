2d89bbf5c357c21912513a3bfff3b070732ceba5
nnqq <43930873+nnqq@users.noreply.github.com>
2020-08-27 12:26:32 +0300

    wip

--- stan/client.go
+++ stan/client.go
@@ -5,32 +5,10 @@ import (
 	s "github.com/nats-io/stan.go"
 	"github.com/nnqq/scr-parser/config"
 	"github.com/nnqq/scr-parser/logger"
-	"os"
 	"strings"
-	"syscall"
-	"time"
 )
 
 var Conn s.Conn
-
-// Sometimes at CPU spikes parser can't reply on STAN heartbeat
-// and STAN removes it from alive clients.
-// But probes is OK, and k8s deployment is alive.
-// We poll status and rollout deployment when connection is lost.
-func pollAlive(sc s.Conn) {
-	t := time.NewTicker(time.Second)
-	defer t.Stop()
-
-	for {
-		<-t.C
-
-		if sc.NatsConn().IsClosed() {
-			p, err := os.FindProcess(os.Getpid())
-			logger.Must(err)
-			logger.Must(p.Signal(syscall.SIGTERM))
-		}
-	}
-}
 
 func connect() (sc s.Conn, err error) {
 	return s.Connect(
@@ -47,5 +25,4 @@ func init() {
 	var err error
 	Conn, err = connect()
 	logger.Must(err)
-	go pollAlive(Conn)
 }
