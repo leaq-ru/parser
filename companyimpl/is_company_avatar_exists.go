4c53114e18f1e1e2c150c870b4d6982a835e290f
nnqq <43930873+nnqq@users.noreply.github.com>
2020-10-03 19:37:00 +0300

    wip

---
+++ companyimpl/is_company_avatar_exists.go
@@ -0,0 +1,39 @@
+package companyimpl
+
+import (
+	"context"
+	"errors"
+	"github.com/nnqq/scr-parser/logger"
+	"github.com/nnqq/scr-parser/mongo"
+	"github.com/nnqq/scr-proto/codegen/go/parser"
+	"go.mongodb.org/mongo-driver/bson"
+	m "go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
+
+func (s *server) IsCompanyAvatarExists(ctx context.Context, req *parser.IsCompanyAvatarExistsRequest) (
+	res *parser.IsCompanyAvatarExistsResponse,
+	err error,
+) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	res = &parser.IsCompanyAvatarExistsResponse{}
+
+	errFindOne := mongo.Companies.FindOne(ctx, bson.M{
+		"a": req.GetAvatarS3Url(),
+	}).Err()
+	if errFindOne != nil {
+		if errors.Is(errFindOne, m.ErrNoDocuments) {
+			res.IsExists = false
+			return
+		} else {
+			err = errFindOne
+			logger.Log.Error().Err(err).Send()
+			return
+		}
+	}
+
+	res.IsExists = true
+	return
+}
