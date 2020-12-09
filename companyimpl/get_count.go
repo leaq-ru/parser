e78216854fb64873016a0e5073e6334bdf30aa80
nnqq <43930873+nnqq@users.noreply.github.com>
2020-12-10 00:25:28 +0300

    wip

---
+++ companyimpl/get_count.go
@@ -0,0 +1,37 @@
+package companyimpl
+
+import (
+	"context"
+	"github.com/nnqq/scr-parser/logger"
+	"github.com/nnqq/scr-parser/mongo"
+	"github.com/nnqq/scr-proto/codegen/go/parser"
+	"time"
+)
+
+func (s *server) GetCount(
+	ctx context.Context,
+	req *parser.GetV2Request,
+) (
+	res *parser.GetCountResponse,
+	err error,
+) {
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	query, err := makeGetQuery(req)
+	if err != nil {
+		logger.Log.Error().Err(err).Send()
+		return
+	}
+
+	count, err := mongo.Companies.CountDocuments(ctx, query)
+	if err != nil {
+		logger.Log.Error().Err(err).Send()
+		return
+	}
+
+	res = &parser.GetCountResponse{
+		Count: uint32(count),
+	}
+	return
+}
